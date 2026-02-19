88ed798fc8f1aa78b7db3d914236bba86c766b39
Coco10023 <mauricio2003@live.se>
2026-02-19 20:40:51 +0100

    Fixar githubs action med workflow för att lägga upp hemsidan

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         index: resolve(__dirname, "src/pages/index.html"),
         animering: resolve(__dirname, "src/pages/animering.html"),
+        base: "/laboration-4/"
       },
     },
   },
